18a8347ff617106320ecada7ec5d89d65312db06
DA5Hx <tanishq.31577@gmail.com>
2021-07-30 20:11:15 +0530

    modified app.js and ejs page

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-const app = require('express');
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+    res.render("home");
+});
+
+app.listen(3000, function() {
+    console.log("Server is running at port 3000");
+});
