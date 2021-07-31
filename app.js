bc837331f5f9a15c748de4d67fc7752cf7c0610c
DA5Hx <tanishq.31577@gmail.com>
2021-07-31 20:05:01 +0530

    uploaded

--- app.js
+++ app.js
@@ -8,8 +8,78 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const arr = [{
+        university: "AUSTRALIAN NATIONAL UNIVERSITY",
+        country: "Australia",
+        expense: "10-12 Lakhs",
+        deadline: "Sept 15,2019"
+    },
+    {
+        university: "THE UNIVERSITY OF MELBOURNE",
+        country: "Australia",
+        expense: "10-12 Lakhs",
+        deadline: "Sept 15, 2019"
+    },
+    {
+        university: "UNIVERSITY OF COMBODIA",
+        country: "Canada",
+        expense: "10-12 Lakhs",
+        deadline: "Sept 15, 2019"
+    },
+    {
+        university: "UNSW SYDNEY",
+        country: "Australia",
+        expense: "10-12 Lakhs",
+        deadline: "Sept 15,2019"
+    },
+    {
+        university: "THE UNIVERSITY IF EDINBURGH",
+        country: "United Kingdom",
+        expense: "15 Lakhs",
+        deadline: "Sept 15, 2019"
+    },
+    {
+        university: "THE UNIVERSITY OF MANCHESTER",
+        country: "United Kingdom",
+        expense: "15 Lakhs",
+        deadline: "Sept 15, 2019"
+    },
+    {
+        university: "UNIVERSITY OF OXFORD",
+        country: "United Kingdom",
+        expense: "12-15 Lakhs",
+        deadline: "Sept 15, 2019"
+    },
+    {
+        university: "UNIVERSITY OF MUNICH",
+        country: "Germany",
+        expense: "12 Lakhs",
+        deadline: "Sept 15, 2019"
+    },
+    {
+        university: "UNIVERSITY OF SOUTHERN CALIFORNIA",
+        country: "United States",
+        expense: "12-15 Lakhs",
+        deadline: "Sept 15, 2019"
+    },
+    {
+        university: "UNIVERSITY OF WESTERN AUSTRALIA",
+        country: "Australia",
+        expense: "15 Lakhs",
+        deadline: "Sept 15, 2019"
+    },
+];
+var numberofcards = 6;
 app.get("/", function(req, res) {
-    res.render("home");
+    res.render("home", {
+        c: numberofcards,
+        array: arr
+    });
+});
+app.post("/", function(req, res) {
+    if (numberofcards < 10)
+        numberofcards++;
+    res.redirect("/");
 });
 
 app.listen(3000, function() {
